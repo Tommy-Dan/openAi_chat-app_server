6d50a6a129b6b2d66faa7ebdedcf9b822004178d
Tommy-Dan <ttuckerwra@gmail.com>
2023-09-21 17:05:08 +0000

    first commit

---
+++ routes/openai-3.0.js
@@ -0,0 +1,105 @@
+/* 
+  NOTE: replace the openai.js file with this file and uncomment 
+  the code if you want to use the newer version of the openai API.
+  OPENAI released their gpt-3.5-turbo version on 3/1/2023, this is
+  gpt-3.5 version which is what powers the ChatGPT bot. most of the
+  code is the same with some minor changes.
+*/
+
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+import { openai } from "../index.js";
+
+dotenv.config();
+const router = express.Router();
+
+router.post("/text", async (req, res) => {
+  try {
+    const { text, activeChatId } = req.body;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: text,
+      temperature: 0.5,
+      max_tokens: 2048,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    await axios.post(
+      `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+      { text: response.data.choices[0].text },
+      {
+        headers: {
+          "Project-ID": process.env.PROJECT_ID,
+          "User-Name": process.env.BOT_USER_NAME,
+          "User-Secret": process.env.BOT_USER_SECRET,
+        },
+      }
+    );
+
+    res.status(200).json({ text: response.data.choices[0].text });
+  } catch (error) {
+    console.error("error", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/code", async (req, res) => {
+  try {
+    const { text, activeChatId } = req.body;
+
+    const response = await openai.createCompletion({
+      model: "code-davinci-002",
+      prompt: text,
+      temperature: 0.5,
+      max_tokens: 2048,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    await axios.post(
+      `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+      { text: response.data.choices[0].text },
+      {
+        headers: {
+          "Project-ID": process.env.PROJECT_ID,
+          "User-Name": process.env.BOT_USER_NAME,
+          "User-Secret": process.env.BOT_USER_SECRET,
+        },
+      }
+    );
+
+    res.status(200).json({ text: response.data.choices[0].text });
+  } catch (error) {
+    console.error("error", error.response.data.error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/assist", async (req, res) => {
+  try {
+    const { text } = req.body;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `Finish my thought: ${text}`,
+      temperature: 0.5,
+      max_tokens: 1024,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).json({ text: response.data.choices[0].text });
+  } catch (error) {
+    console.error("error", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+export default router;
+
